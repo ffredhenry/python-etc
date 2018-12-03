4cf1969e987918f98c05326180570cf83b5fa3e4
fhenry9618 <f.fredhenry@gmail.com>
2018-12-03 13:52:07 -0600

    making each subcomponent into a full file, will merge when finished.

---
+++ directory_finder.py
@@ -0,0 +1,10 @@
+#This portion of code finds a target directory, and leaves a "marker file" there signifying success
+#Additionally, it tabs-to-complete during the directory selection phase of the program
+#if a directory is not available, it sends an error to the console
+#marker file is called "foundit.txt" and contains the message 'Tallyho, directory_finder.py was here!'
+
+import os
+import sys
+import glob
+
+print("yeet!")
