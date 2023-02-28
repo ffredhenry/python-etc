8e276b064e71e3da7d128e82aec2a6f0a726b4f1
Fred Henry <24500479+internetlandlord@users.noreply.github.com>
2023-02-27 20:34:36 -0600

    Add files via upload
    adding a regex test

---
+++ regex_phone.py
@@ -0,0 +1,28 @@
+# basic password hasher and keeper
+
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0,3):
+        if not text[i].isdecimal():
+            return False
+        if text[3] != "-":
+            return False
+    for i in range(4,7):
+        if not text[i].isdecimal():
+            return False
+        if text[7] != "-":
+            return False
+    for i in range(8,12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+#main
+while True:
+    print("Enter a phone number")
+    userPN = input()
+
+    if isPhoneNumber(userPN):
+        break
+print("end")
